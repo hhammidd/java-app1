0fe5749ece584bd002acdf87aaab761e750fb381
Ham Shafi <hamid.shafikani@gmail.com>
2018-10-02 10:27:23 +0200

    new controller vpvx

---
+++ src/main/java/com/hamidApp1/controller/pv/PvController.java
@@ -0,0 +1,22 @@
+package com.hamidApp1.controller.pv;
+
+
+import com.hamidApp1.model.pv.Pv;
+import com.hamidApp1.service.pv.PvService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pv")
+public class PvController {
+    @Autowired
+    private PvService pvService;
+    @GetMapping(value = "/findAll")
+    public List<Pv> getAllPv()  {
+        return pvService.findAll();
+    }
+}
