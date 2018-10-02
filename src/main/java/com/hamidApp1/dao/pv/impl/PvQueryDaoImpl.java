f96a978d8842dab3cb972bbcd1a330c54a54ee2c
Ham Shafi <hamid.shafikani@gmail.com>
2018-10-02 10:19:51 +0200

    new controller vpv

---
+++ src/main/java/com/hamidApp1/dao/pv/impl/PvQueryDaoImpl.java
@@ -0,0 +1,20 @@
+package com.hamidApp1.dao.pv.impl;
+
+import com.hamidApp1.dao.permissions.PermissionsDao;
+import com.hamidApp1.dao.pv.PvDao;
+import com.hamidApp1.dao.pv.PvQueryDao;
+import com.hamidApp1.model.pv.Pv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class PvQueryDaoImpl implements PvQueryDao {
+
+    @Autowired
+    private PvDao pvDao;
+    @Override
+    public List<Pv> findAll() {
+        return pvDao.findAll();
+    }
+}
