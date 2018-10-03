a4f936836e4a487c31996a53c6836b7ffd359e1b
Ham Shafi <hamid.shafikani@gmail.com>
2018-10-03 12:04:49 +0200

    new controller vpvx

---
+++ src/main/java/com/hamidApp1/dao/istat/impl/IstatItQueryDaoImpl.java
@@ -0,0 +1,22 @@
+package com.hamidApp1.dao.istat.impl;
+
+import com.hamidApp1.dao.istat.IstatItDao;
+import com.hamidApp1.dao.istat.IstatItQueryDao;
+import com.hamidApp1.dao.permissions.PermissionsDao;
+import com.hamidApp1.model.istat.Istat_it;
+import com.hamidApp1.model.permissions.Permissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class IstatItQueryDaoImpl implements IstatItQueryDao {
+    @Autowired
+    private IstatItDao istatItDao;
+
+    @Override
+    public List<Istat_it> findAll() {
+        return istatItDao.findAll();
+    }
+}
